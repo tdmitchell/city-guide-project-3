22287bc52b5e5ba6a64ebddfb9052594339aab67
TheoMitchell <theomitchell@gmail.com>
2022-04-29 10:21:02 -0400

    3. Test deployment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>City Information Guide</h1>
+        <h2>Coming Soon!</h2>
+        <h3>Developed by Theo Mitchell</h3>
       </header>
     </div>
   );
